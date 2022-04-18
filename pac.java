a5b631c8f03865d17498ae5fb11c5b0e480e4005
aldibhaz <103481339+aldibhaz@users.noreply.github.com>
2022-04-18 15:16:00 +0700

    komit pertama

---
+++ pac.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class pac extends JFrame{
+
+    public pac() {
+        add(new Model());
+    }
+    public static void main(String[] args) {
+        Pacman pac = new Pacman();
+        pac.setVisible(true);
+        pac.setTitle("Pacman");
+        pac.setSize(380,420);
+        pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pac.setLocationRelativeTo(null);
+
+    }
+
+}
